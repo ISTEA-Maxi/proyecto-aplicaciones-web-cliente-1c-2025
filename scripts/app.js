1bf4285507f555427648c9c66b7d509648b5b5d5
MaxiTutor <maximiliano.tutor@istea.com.ar>
2025-06-22 14:46:03 -0300

    fix button :)

--- scripts/app.js
+++ scripts/app.js
@@ -2,25 +2,25 @@ const products = [
     {
         name: "Camiseta",
         description: "Camiseta de algodon 100%",
-        image: "/img/google.png",
+        image: "./img/google.png",
         price: 15,
     },
     {
         name: "Pantalon",
         description: "Pantalon largo",
-        image: "/img/google.png",
+        image: "./img/google.png",
         price: 20,
     },
     {
         name: "Zapatos",
         description: "Zapatos de cuero",
-        image: "/img/google.png",
+        image: "./img/google.png",
         price: 30,
     },
     {
         name: "Gorro",
         description: "Gorro de sol",
-        image: "/img/google.png",
+        image: "./img/google.png",
         price: 10,
     },
 ];
@@ -55,7 +55,22 @@ function createproductcard(products) {
     return card; /*retornamos la tarjeta*/
 }
 
+// Funcion para agregar un nuevo producto
+function addProduct() {
+    const newProduct = {
+        name: "Nuevo Producto",
+        description: "Descripcion del nuevo producto",
+        image: "./img/google.png",
+        price: 25,
+    };
+    const card = createproductcard(newProduct); /*creamos la tarjeta del nuevo producto*/
+    grid.appendChild(card); /*insertamos la tarjeta en el grid*/
+}
+
 products.forEach(product => { /*recorre todo el vector*/
     const card = createproductcard(product); /*creamos la tarjeta*/
     grid.appendChild(card); /*inserta lo productos*/
 });
+
+const button = document.querySelector('#btn-add-product');
+button.addEventListener('click', addProduct); 
